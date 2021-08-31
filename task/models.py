779ac683ecc2e141a216d9f026c694e3d70c3ccb
Heaciy <duke3247561384@icloud.com>
2021-08-31 09:22:18 +0000

    为TaskResult、PeriodicTask添加动态绑定

--- task/models.py
+++ task/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_celery_results.models import TaskResult
+from django_celery_beat.models import PeriodicTask
+from .utils import load_from_kwargs
 
-# Create your models here.
+
+# class ExtraTaskResultInfo(models.Model):
+#     task_result = models.OneToOneField(TaskResult, on_delete=models.CASCADE)
+#     task_creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+# class ExtraTaskBeatInfo(models.Model):
+#     task_beat = models.OneToOneField(PeriodicTask,on_delete=models.CASCADE)
+#     task_creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+@property
+def owner(self):
+    uid = load_from_kwargs(self, 'uid')
+    return User.objects.filter(id=int(uid)).first() if uid else None
+
+
+@property
+def task_obj(self):
+    tid = load_from_kwargs(self, 'tid')
+    return PeriodicTask.objects.filter(id=int(tid)).first() if tid else None
+
+# 动态绑定
+PeriodicTask.owner = owner
+TaskResult.owner = owner
+TaskResult.task_obj = task_obj
