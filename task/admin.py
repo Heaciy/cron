04b83db200eda7a8fce29d5c991f54df1d6413fd
Heaciy <duke3247561384@icloud.com>
2021-08-31 09:58:16 +0000

    为TaskResult、PeriodicTask动态绑定方法

--- task/admin.py
+++ task/admin.py
@@ -1,4 +1,3 @@
-import json
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django_celery_beat.models import PeriodicTask
@@ -7,37 +6,12 @@ from django_celery_results.models import TaskResult
 from django_celery_results.admin import TaskResultAdmin
 
 
-class ExtraAdmin(admin.ModelAdmin):
-    @staticmethod
-    def load_from_kwargs(obj, key):
-        # 使用getattr同时适配TaskResult和PeriodicTask
-        kwargs_str = getattr(obj, "task_kwargs") if hasattr(obj, "task_kwargs")\
-            else getattr(obj, "kwargs")
-        kwargs = json.loads(kwargs_str.strip("\"").replace('\'', '\"'))
-        return kwargs.get(key, None)
-
-    def get_owner(self, obj):
-        uid = self.load_from_kwargs(obj, 'uid')
-        if uid:
-            return User.objects.filter(id=int(uid)).first().username
-        return "None"
-    get_owner.short_description = 'Owner'
+class ExtraTaskResultAdmin(TaskResultAdmin):
+    list_display = ('task_id', 'task_name', 'task_obj', 'owner', 'date_done', 'status', 'worker')
 
 
-class ExtraTaskResultAdmin(ExtraAdmin, TaskResultAdmin):
-    list_display = ('task_id', 'task_name', 'get_owner',
-                    'get_task', 'date_done', 'status', 'worker')
-
-    def get_task(self, obj):
-        tid = self.load_from_kwargs(obj, 'tid')
-        if tid:
-            return PeriodicTask.objects.filter(id=int(tid)).first().name
-        return ""
-    get_task.short_description = 'TaskBeat'
-
-
-class ExtraPeriodicTaskAdmin(ExtraAdmin, PeriodicTaskAdmin):
-    list_display = ('__str__', 'get_owner', 'enabled', 'interval', 'start_time',
+class ExtraPeriodicTaskAdmin(PeriodicTaskAdmin):
+    list_display = ('__str__', 'owner', 'enabled', 'interval', 'start_time',
                     'last_run_at', 'one_off')
 
 
--- task/models.py
+++ task/models.py
@@ -1,16 +1,23 @@
 from django.contrib.auth.models import User
 from django_celery_results.models import TaskResult
 from django_celery_beat.models import PeriodicTask
+from django.contrib import admin
 from .utils import load_from_kwargs
 
 
+# TODO: 分别处理TaskResult和PeriodicTask的kwargs
+# 从中解析出uid和tid再使用display和Concat组合进行排序
+# https://docs.djangoproject.com/en/3.2/ref/models/database-functions/#concat
+# https://docs.djangoproject.com/zh-hans/3.2/ref/contrib/admin/#django.contrib.admin.display
 @property
+@admin.display(description='owner')
 def owner(self):
     uid = load_from_kwargs(self, 'uid')
     return User.objects.filter(id=int(uid)).first() if uid else None
 
 
 @property
+@admin.display(ordering='task_kwargs', description='task obj')
 def task_obj(self):
     tid = load_from_kwargs(self, 'tid')
     return PeriodicTask.objects.filter(id=int(tid)).first() if tid else None
