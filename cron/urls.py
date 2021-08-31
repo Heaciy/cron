681511ea46078824b6c78ca06514a6689caa4986
Heaciy <duke3247561384@icloud.com>
2021-08-31 13:13:24 +0000

    add django-debug-tool

--- cron/urls.py
+++ cron/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
@@ -20,5 +21,11 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('task/',include('task.urls')),
+    path('task/', include('task.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
