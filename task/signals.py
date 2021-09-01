5898954ce2ec49b60e02982b364a8916ee64c89d
Heaciy <duke3247561384@icloud.com>
2021-09-01 19:34:01 +0000

    添加clean_schedule方法用于自动清理schedule

--- task/signals.py
+++ task/signals.py
@@ -9,10 +9,19 @@ print("=========signal on task=========")
 
 @receiver(models.signals.post_save, sender=PeriodicTask)
 def decorate_kwargs(sender, instance, **kwargs):
-    print(f"instance:{instance}")
-    print(f"kwargs:{kwargs}")
+    """在创建、保存PeriodicTask时自动为其kwargs添加tid字段"""
     kwargs = load_kwargs_from_str(instance.kwargs)
     if 'tid' not in kwargs:
         kwargs.update({'tid': instance.id})
         instance.kwargs = dumps_kwargs_safe(kwargs)
         instance.save()
+
+
+@receiver(models.signals.post_delete, sender=PeriodicTask)
+def clean_schedule(sender, instance, **kwargs):
+    """删除PeriodicTask时自动清理相关联的schedule"""
+    schedules = [instance.interval, instance.crontab,
+                 instance.solar, instance.clocked]
+    for schedule in schedules:
+        if schedule and schedule.periodictask_set.all().count() == 0:
+            schedule.delete()
