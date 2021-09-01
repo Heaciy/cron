722fa13c4dbd1a48f7be7ba5d250d40d4dab25b1
Heaciy <duke3247561384@icloud.com>
2021-09-01 19:16:00 +0000

    添加generic测试方法

--- task/tasks.py
+++ task/tasks.py
@@ -1,9 +1,17 @@
 from celery import shared_task
+from django.contrib.auth.models import User
 import time
 
 
 @shared_task
-def add(x, y):
-    print("任务函数add 正在执行...")
+def add(x, y,*args,**kwargs):
+    print(kwargs)
+    user = User.objects.get(id=kwargs.get('uid',1))
+    print(f"任务函数add 正在执行...\n用户{user.username}")
     time.sleep(2)
     return x + y
+
+@shared_task
+def generic(*args,**kwargs):
+    print("generic task running...")
+    print(f"args:\t{args}\nkwargs:\t{kwargs}")
