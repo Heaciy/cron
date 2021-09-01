e2d1264aaa689eff7797cbf784ab138211c8215f
Heaciy <duke3247561384@icloud.com>
2021-09-01 20:09:13 +0000

    删除无用的forms

--- task/forms.py
+++ task/forms.py
@@ -1,8 +1,2 @@
-from os import WEXITED
 from django import forms
-
-class DailyCPTaskForm(forms.Form):
-    username = forms.CharField(label='学号',max_length=20,widget=forms.TextInput)
-    password = forms.CharField(label='密码',max_length=20,widget=forms.PasswordInput)
-    crontab = forms.CharField(label='时间',max_length=20,widget=forms.TextInput)
     
